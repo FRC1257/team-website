88befa5a5f26bbc6d5e3a2ce367b86a309242b21
Zach Breit <zbreit18@gmail.com>
2016-07-24 15:16:02 -0400

    Formatted the html and fixed up media queries

--- js/ticker.js
+++ js/ticker.js
@@ -8,14 +8,13 @@ var ticker_id_for_tba = 'frc1257:websiteticker:test';
 
 xhr.open('GET', 'https://www.thebluealliance.com/api/v2/team/frc' + team_number + '/' + year + '/events?X-TBA-App-Id=' + ticker_id_for_tba, true);
 xhr.send();
- 
+
 var response = "";
 xhr.onreadystatechange = processRequest;
- 
+
 function processRequest(e) {
  if (xhr.readyState == 4 && xhr.status == 200) {
         response = JSON.parse(xhr.responseText);
-
     }
 }
 
