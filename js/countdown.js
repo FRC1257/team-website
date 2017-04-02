f9a2cf80552592d6ecc4c8b74286f986f0bac970
Zach Breit <zbreit18@gmail.com>
2017-04-02 09:21:50 -0400

    Added new events to the countdown timer

--- js/countdown.js
+++ js/countdown.js
@@ -5,7 +5,7 @@ var countdown = document.getElementById('countdown');
 
 function postEventInProgress(event) {
     countdown.innerHTML = "<span>We are currently competing in " + event.atag + "!</span>";
-    countdown.parentElement.style.background="#5aad62";
+    countdown.parentElement.style.background = "#5aad62";
 }
 
 function addCountdown(futureDate) {
@@ -22,16 +22,16 @@ function addCountdown(futureDate) {
     minutes = parseInt(seconds_left / 60);
     seconds = parseInt(seconds_left % 60);
 
-    countdown.innerHTML = '<span class="days"><strong>' + days +  '</strong> Days</span> <span class="hours"><strong>' + hours + '</strong> Hours</span> <span class="minutes"><strong>' + minutes + '</strong> Minutes</span> <span class="seconds"><strong>' + seconds + '</strong> Seconds</span>';
+    countdown.innerHTML = '<span class="days"><strong>' + days + '</strong> Days</span> <span class="hours"><strong>' + hours + '</strong> Hours</span> <span class="minutes"><strong>' + minutes + '</strong> Minutes</span> <span class="seconds"><strong>' + seconds + '</strong> Seconds</span>';
 }
 
 class event {
     constructor(name, start, end, link) {
-        this.name  = name;
+        this.name = name;
         this.start = start;
-        this.end   = end;
-        this.link  = link;
-        this.atag  = '<a target="_blank" href="' + link + '"><strong>' + name + '</strong></a>'
+        this.end = end;
+        this.link = link;
+        this.atag = '<a target="_blank" href="' + link + '"><strong>' + name + '</strong></a>'
     }
 
     post() {
@@ -39,48 +39,49 @@ class event {
     }
 }
 
-
 /* Main */
 var events = [];
 var eventPosted = false;
 
 events.push(new event(
-                'Hatboro-Horsham',
-                new Date('Mar, 3, 2017, 8:00:00 GMT-400').getTime(),
-                new Date('Mar, 5, 2017, 8:00:00 GMT-400').getTime(),
-                'https://www.thebluealliance.com/event/2017pahat/'
-                ));
+    'MAR District Championship',
+    new Date('Apr, 5, 2017, 8:00:00 GMT-400').getTime(),
+    new Date('Apr, 8, 2017, 18:00:00 GMT-400').getTime(),
+    'https://www.thebluealliance.com/event/2017mrcmp'
+));
 
 events.push(new event(
-                'Bridgewater-Raritan',
-                new Date('Mar, 17, 2017, 8:00:00 GMT-400').getTime(),
-                new Date('Mar, 19, 2017, 8:00:00 GMT-400').getTime(),
-                'https://www.thebluealliance.com/event/2017njbri'
-                ));
-
-
+    'FIRST Championship (St. Louis)',
+    new Date('Apr, 26, 2017, 8:00:00 GMT-400').getTime(),
+    new Date('Apr, 29, 2017, 18:00:00 GMT-400').getTime(),
+    'https://www.thebluealliance.com/event/2017cmpmo'
+));
 
 //Sort the events array based on start date
-events.sort(function(a,b) {return a.start - b.start; });
+events.sort(function(a, b) {
+    return a.start - b.start;
+});
 
-for(var currentEvent in events) {
+for (var currentEvent in events) {
 
     //If the currentEvent in the array hasn't occured yet,
-    if(events[currentEvent].start > currentDate) {
+    if (events[currentEvent].start > currentDate) {
         document.getElementById("nextevent").innerHTML = 'Next Event: <strong>' + events[currentEvent].atag + '</strong>';
-        setInterval(function() { events[currentEvent].post(); }, 1000);
+        setInterval(function() {
+            events[currentEvent].post();
+        }, 1000);
         eventPosted = true;
         break;
     } //If this statement is true, the start of the event has passed
 
     //If the event is in progress, post it as such
-    else if(events[currentEvent].end > currentDate) {
+    else if (events[currentEvent].end > currentDate) {
         postEventInProgress(events[currentEvent]);
         eventPosted = true;
         break;
     }
 }
 
-if(!eventPosted) {
+if (!eventPosted) {
     document.getElementById("nextevent").innerHTML = '<span>We have no upcoming events!</span>';
 }
